06a51175e8e8fbc0afddd9aa9f9f111692bd7afc
Theodore R. Smith <thsmith@shift4.com>
2022-07-27 14:15:14 -0500

    MGO-158: Fixed the magento autodeploy.

--- scripts/afterdeploy.sh
+++ scripts/afterdeploy.sh
@@ -5,6 +5,8 @@
 rm -fr /var/www/magento/public_html/pub/static/frontend/
 rm -fr /var/www/magento/public_html/pub/static/adminhtml/
 rm -fr /var/www/magento/public_html/var/cache/
-/usr/bin/php7.4 /var/www/magento/public_html/bin/magento setup:static-content:deploy -f
+/usr/bin/php7.4 /var/www/magento/public_html/bin/magento setup:upgrade
 /usr/bin/php7.4 /var/www/magento/public_html/bin/magento setup:di:compile
+/usr/bin/php7.4 /var/www/magento/public_html/bin/magento setup:static-content:deploy -f
+/usr/bin/php7.4 /var/www/magento/public_html/bin/magento cache:clean
 chmod -R 777 /var/www/magento/public_html/
